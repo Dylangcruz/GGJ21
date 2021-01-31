89f5c668b75ae938c5e475e05424e99def730056
Dylan Cruz <dylan.g.cruz.fonseca@gmail.com>
2021-01-30 20:54:13 -0400

    Better Movement + eggy Model

--- GGJ21/Assets/Scripts/PlayerMovement.cs
+++ GGJ21/Assets/Scripts/PlayerMovement.cs
@@ -4,32 +4,69 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public int speed = 5;
-    // Start is called before the first frame update
-    void Start()
+
+    Vector3 up = Vector3.zero,
+            right = new Vector3(0, 90, 0),
+            down = new Vector3(0, 180, 0),
+            left = new Vector3(0, 270, 0),
+            currentDirection = Vector3.zero;
+
+    Vector3 nextPos, destination, direction;
+    public float speed = 5f;
+
+    private void Start()
     {
-        
+        currentDirection = up;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Move();
+    }
 
+    void Move()
+    {
+        int horizontalValue = 0;
+        int verticalValue = 0;
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position += Vector3.forward * speed * Time.deltaTime;
+            verticalValue += 1;
+            currentDirection = up;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            verticalValue -= 1;
+            currentDirection = down;
+
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position += Vector3.left * speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.position += Vector3.back  * speed * Time.deltaTime;
+            horizontalValue -= 1;
+            currentDirection = left ;
+
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position += Vector3.right * speed * Time.deltaTime;
+            horizontalValue += 1;
+            currentDirection = right;
+
         }
+
+
+
+        //redirect model
+        transform.localEulerAngles = currentDirection;
+        //move
+
+        Vector3 movementVector = (Vector3.right * horizontalValue +
+                                  Vector3.forward * verticalValue).normalized;
+        GetComponent<Rigidbody>().velocity = movementVector * speed;
+        
+        Debug.Log(GetComponent<Rigidbody>().velocity);
+
     }
+    
+
 }
